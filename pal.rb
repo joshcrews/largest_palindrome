52be44a48ddecb0008ad5de464477b318618d9a1
Josh Crews <josh@joshcrews.com>
2015-11-25 19:44:43 -0600

    just reformatting changes

--- pal.rb
+++ pal.rb
@@ -5,7 +5,7 @@ class Pal
 		num_str == num_str.reverse
 	end
 
-	def self.has_factors_in_range_and_given_length? num, upper_bound, length
+	def self.has_factors_in_range_and_given_length?(num, upper_bound, length)
 		upper_bound.downto(Math.sqrt(num).to_i) do |divisor|
 			if ((num % divisor == 0) && (num / divisor).to_s.length == length)
 				return true
@@ -14,26 +14,24 @@ class Pal
 		false
 	end
 
-	def self.find_largest_palindrome n
+	def self.find_largest_palindrome(number)
 
-		lower_bound = 10**(n-1)
-		upper_bound = 10**(n) -1
+		lower_bound = 10 ** (number - 1)
+		upper_bound = 10 ** (number) -1
 
-		(upper_bound**2).downto(lower_bound**2) do |num|
-			if ((is_palindrome? num) && has_factors_in_range_and_given_length?(num, upper_bound, n))
-				return num
+		puts lower_bound
+		puts upper_bound
+
+		(upper_bound ** 2).downto(lower_bound ** 2) do |trial_number|
+			puts trial_number
+			if is_palindrome?(trial_number)
+				puts "  #{trial_number}"
+				if has_factors_in_range_and_given_length?(trial_number, upper_bound, number)
+					return trial_number
+				end
 			end
 		end
 		nil
 	end
 
 end
-
-
-
-
-
-
-
-
-
